a14a44a2a4473185d1a073a5ca0b07f37c55b6d0
Beefywhale <beefywhale@gmail.com>
2017-12-25 21:30:59 -0600

    *thinking emoji*

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -963,15 +963,14 @@ void CCharacter::Tick()
 					pChr->m_Killer.m_KillerID = iHookedPlayer;
 
 					if (pChr->IsFreezed()) {
-						m_shouldCountHooks = false;
-					}
-					else {
 						m_shouldCountHooks = true;
 						return;
+					} else {
+						m_shouldCountHooks = false;
 					}
 
 					//Beefywhale's hook block detection | check if player has grappled
-					if (!pChr->IsFreezed() && m_shouldCountHooks && m_Core.m_HookState == 0 && m_Core.m_HookTick <= Server()->TickSpeed() / (1000.f / (float)1000)) {
+					if (m_shouldCountHooks && m_Core.m_HookState == 0 && m_Core.m_HookTick <= Server()->TickSpeed() / (1000.f / (float)1000)) {
 						m_hooks += 1;
 						m_Killer.m_uiKillerHookTicks = 0;
 						if (m_hooks > g_Config.m_SvHookblockMaxSpam) {
@@ -983,7 +982,7 @@ void CCharacter::Tick()
 						}
 					}
 					//Beefywhale's hook block detection | Check if player is holding another for more than (1) second as long as the person they're grabbing isn't frozen, than freeze them.
-					else if (!pChr->IsFreezed() && m_shouldCountHooks && g_Config.m_SvHookblock && m_Core.m_HookTick >= Server()->TickSpeed() / (1000.f / (float)g_Config.m_SvHookblockHookTime)) {
+					else if (m_shouldCountHooks && g_Config.m_SvHookblock && m_Core.m_HookTick >= Server()->TickSpeed() / (1000.f / (float)g_Config.m_SvHookblockHookTime)) {
 						char buf[256] = { 0 };
 						snprintf(buf, sizeof(buf), "\'%s\' is blocking his teammate \'%s\'", ID_NAME(GetPlayer()->GetCID()), ID_NAME(pChr->GetPlayer()->GetCID()));
 						GameServer()->SendChat(-1, CGameContext::CHAT_ALL, buf);
